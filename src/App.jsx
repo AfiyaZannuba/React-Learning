9b0930793ece810e1002a665b8a02c54ca944cb1
Afiya Zannuba <afiya.zannuba@terrificminds.com>
2024-10-28 12:24:27 +0530

    Learning JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import WelcomeMessage from "./components/WelcomeMessage";
+
 
 
 const App = () => {
   return(
     <div>
-      <Header />
-      <Main />
-      <Footer />
+      <WelcomeMessage />
     </div>
   )
 }
