a90123a0e82a977c3cfc8b87c3b8bc5c7983ce00
Afiya Zannuba <afiya.zannuba@terrificminds.com>
2024-10-28 13:12:59 +0530

    You can write expressions inside curly braces. Expressions can be React variables, property or any other Javascript expression. JSX will execute the expression and return the result.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import JSXRules from "./components/JSXRules";
 
 const App = () => {
+  const myName = "Afiya Zannuba";
+  const myFriends = [ 'Afiya', 'Alex', 'John'];
+  const multiply = (a,b) => a*b;
+  const specialClass = "simple-class";
+
   return (
-    <div>
-      <JSXRules />
-    </div>
+    <section>
+      <p>2 + 2 = {2 + 2}</p>
+      <h1>{myName}</h1>
+      <p>My Friends List : {myFriends}</p>
+      <p>Multiply 2*2 = {multiply(2,2)}</p>
+      <p className={specialClass}>This is special class</p>
+    </section>
   )
 }
 
