13bcacb7f9d82557b0465acc6885bbccdc42334c
Afiya Zannuba <afiya.zannuba@terrificminds.com>
2024-10-28 14:07:03 +0530

    Destructuring the elements in an array using map method.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-// import Greeting from "./components/Greeting";
-// import ProductInfo from "./components/ProductInfo";
+import UserList from "./components/userList";
 
 const App = () => {
-  const userInfo = [
-    {
-      username: "Afiya",
-      email: "afiya.zannuba@gmail.com",
-      location: "Palakkad"
-    },
-    {
-      username: "Zannuba",
-      email: "zannuba.afiya@gmail.com",
-      location: "Kakkanad"
-    },
-    {
-      username: "Jaleel",
-      email: "jaleel@gmail.com",
-      location: "Kochi"
-    }
-  ]
-
   return (
-    <main>
-      {userInfo.map(({username, email, location}) => (
-        <ul key={Math.random()}>
-          <li>{username}</li>
-          <li>{email}</li>
-          <li>{location}</li>
-        </ul>
-      ))}
-    </main>
+    <UserList />
   )
 }
 
