4184c0dac14183ccfebb0be50ff6eeb6731f57d8
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-05 17:29:39 +0530

    Example programs of useState using numbers, arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-const Button = () => {
-  const handleClick = () => console.log(Math.round(Math.random() * 10));
-  return <button onClick={handleClick}>Click Me</button>
-}
+// State is a way to store and manage data that can change over time 
+// and affects how the component renders. We define state using useState hook, 
+//which allows you to set an intial value and provides a way to update that state.
 
-const Copy = () => {
-  const handleCopy = () => console.log(Math.round(Math.random()*10))
-  return(
-    <p onCopy={handleCopy}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolor quos repudiandae reiciendis tempora ducimus voluptatibus voluptates laudantium nobis aliquam.</p>
-  )
-}
-
-const  Move = () => {
-  const handleMove = () => alert("Mouse move event fired");
-  return (
-    <p onMouseMove={handleMove}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, eveniet.</p>
-  )
-}
+import { useState } from "react"
 
 const App = () => {
+
+  const [friends, setFriends] = useState(['Alex', 'Afiya'])
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount (count + 1);
+  }
+  const decrement = () => {
+    if (count>0){
+    setCount (count - 1);
+    }
+  }
+  const AddFriend = () => {
+    setFriends([...friends, "Allen"])
+  }
+  const RemoveFriend = () => {
+    setFriends(friends.filter((f)=> (f != "Allen")))
+  }
+  const UpDatefriend = () => {
+    setFriends(friends.map((f)=>(f==="Afiya" ? "Afiya Zannuba" : f)));
+  }
   return (
     <section>
-      <Button />
-      <Copy />
-      <Move />
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <br />
+      {friends.map((friend)=>(
+        <li key={Math.round(Math.random()*10)}>{friend}</li>
+      ))}
+
+      <button onClick={AddFriend}>Add a New Friend</button>
+      <button onClick={RemoveFriend}>Remove specific Friend</button>
+      <button onClick={UpDatefriend}>Update</button>
     </section>
   )
 }
