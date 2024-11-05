39abeb978b093178bdcf4c121d4d8bae3b864e57
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-05 18:19:33 +0530

    When useState is used as a callback function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-// State is a way to store and manage data that can change over time 
-// and affects how the component renders. We define state using useState hook, 
-//which allows you to set an intial value and provides a way to update that state.
 
-import { useState } from "react"
-import ComponentOne from "./components/ComponentOne"
-import ComponentTwo from "./components/ComponentTwo"
+import ExampleOne from "./components/ExampleOne"
+
 
 const App = () => {
-  const [count, setCount] = useState(0)
   return (
     <section>
-      <ComponentOne count={count} setCount={setCount}/>
-      <ComponentTwo />
+     <ExampleOne />
     </section>
   )
 }
