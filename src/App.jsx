68bbfe40ac891fd2abc5bcc847630cf2b080d39f
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-05 17:57:00 +0530

    Example program of useState for array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,35 @@
 import { useState } from "react"
 
 const App = () => {
-  const [movie, setMovie] = useState({
-    title: "Aram",
-    ratings: 9,
-  })
+  const [movie, setMovie] = useState([
+    {id: 1,
+      title: "Aram",
+      ratings: 5
+    },
+    {id: 2,
+      title: "Spider Man",
+      ratings: 7
+    },
+    {
+      id: 3,
+      title: "Karam",
+      ratings: 10
+    }
+  ])
+
   const handleClick = () => {
-    setMovie({...movie, ratings:5})
+    setMovie(movie.map((m)=>m.id===1 ? {...movie, title: "John Wick 5"} : m ))
+  }
+  const handleAdd = () => {
+    setMovie({...movie, id:4, title: "Malayalam", rating: 10})
   }
   return (
     <section>
-      <h1>Title: {movie.title}</h1>
-      <p>Ratings: {movie.ratings}</p>
-      <button onClick={handleClick}>Update Rating</button>
+      {movie.map((movie)=>(
+        <li key={Math.random()}>{movie.title}</li>
+      ))}
+      <button onClick={handleClick}>Change Name</button>
+      <button onClick={handleAdd}>Add New Movie</button>
     </section>
   )
 }
