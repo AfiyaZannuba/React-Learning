1b01eedc7afa3dd4d9a5e4017854dcb34eb93b95
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-06 13:25:55 +0530

    Implementation of React Portal with example

--- src/components/PopupCotent.jsx
+++ src/components/PopupCotent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
 
 const PopupCotent = ({copied}) => {
-  return (
+  return createPortal(
     <div>
         {copied && (
             <div style={{ position: 'absolute', bottom: '3rem'}}>copied to clipboard</div>
         )}
-    </div>
+    </div>,
+    document.querySelector('#popup-content')
   )
 }
 
