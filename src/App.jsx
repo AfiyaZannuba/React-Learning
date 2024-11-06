521cd4c4127cf7e70b97e5b0fbef531550f0ecd0
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-06 23:31:16 +0530

    useRef example program

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Counterr from "./components/Counterr"
-
-
+import { useRef } from "react"
 const App = () => {
+  const inputElement = useRef(null);
+  const focusInput = () => {
+    inputElement.current.focus();
+    inputElement.current.value = 'Afiya'
+  }
   return (
     <div>
-      <Counterr />
+      <input type="text" ref={inputElement} />
+      <button onClick={()=> focusInput()}>Focus & Write</button>
     </div>
   )
 }
