7312f1dcb790f86df4f6b1d200da4a1554dd28d0
AfiyaZannuba <afiya.zannubaa@gmail.com>
2024-11-06 14:45:39 +0530

    useEffect example program

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import CopyInput from "./components/CopyInput"
-import Counter from "./components/Counter"
-import Switcher from "./components/Switcher"
-import Profile from "./Profile"
-import ShoppingList from "./ShoppingList"
-import Todolist from "./Todolist"
+import { useState, useEffect } from "react"
 
 
 
 
 const App = () => {
+
+  const [value, setValue] = useState(0);
+  const [something, setSomething] = useState(0)
+
+  const handleClick = () => {
+    setValue(value + 1)
+  }
+  const handleClicks = () => {
+    setSomething(something + 1)
+  }
+
+  useEffect(()=>{
+    console.log('call useEffect');
+    document.title= `Increment ${value}`
+  }, [value])
+
   return (
     <section>
-      <Switcher />
+    <h2>{value}</h2>
+    <button onClick={handleClick}>Click Me</button>
+    <button onClick={handleClicks}>Increment Me</button>
     </section>
   )
 }
